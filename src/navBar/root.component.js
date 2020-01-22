e527c05be40b93f2a4af249bde393e1e97693892
Adam Howard <adam@technically-agile.dev>
2020-01-22 13:14:13 +0000

    added new routes in root component for navbar

--- src/navBar/root.component.js
+++ src/navBar/root.component.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { navigateToUrl } from 'single-spa'
 
 const NavBar = () => (
   <nav>
     <div className="nav-wrapper">
-      <a className="brand-logo">
-        <ul className="nav-mobile">
-          <li><a>Home</a></li>
-          <li><a>Angular</a></li>
-        </ul>
-      </a>
+      <a className="brand-logo" href="/" onClick={navigateToUrl}></a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="/" onClick={navigateToUrl}>Home</a></li>
+        <li><a href="/angularjs" onClick={navigateToUrl}>AngularJS</a></li>
+      </ul>
+
     </div>
   </nav>
 )
