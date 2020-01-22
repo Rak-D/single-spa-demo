a46b8ca34acc9b6ded3a9b4e9b8a416e228e9824
Adam Howard <adam@technically-agile.dev>
2020-01-22 11:54:20 +0000

    added root component

--- src/home/root.component.js
+++ src/home/root.component.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   Redirect
-} from 'react-router-dom';
-
-/*
-// This CSS needs to go somewhere
+} from "react-router-dom";
+/* this needs to be somewhere
 .fade-enter {
   opacity: 0;
   z-index: 1;
 }
-
 .fade-enter.fade-enter-active {
   opacity: 1;
   transition: opacity 250ms ease-in;
 }
 */
-
-const AnimationExample = () => {
+const AnimationExample = () => (
   <Router basename="/home">
     <Route
       render={({ location }) => (
@@ -31,25 +27,21 @@ const AnimationExample = () => {
             path="/"
             render={() => <Redirect to="/hsl/10/90/50" />}
           />
-
           <ul style={styles.nav}>
             <NavLink to="/hsl/10/90/50">Red</NavLink>
             <NavLink to="/hsl/120/100/40">Green</NavLink>
             <NavLink to="/rgb/33/150/243">Blue</NavLink>
             <NavLink to="/rgb/240/98/146">Pink</NavLink>
           </ul>
-
-          <div style={styles.context}>
+          <div style={styles.content}>
             <TransitionGroup>
-              {/* 
-                This is no different than other usage of CSS Transition, just make sure to pass 'Location' to 'Switch' so it can match the old location as it animates out
+              {/* this is no different than other usage of CSSTransition, just make sure to pass `location` to `Switch` so it can match the old location as it animates out
               */}
               <CSSTransition key={location.key} classNames="fade" timeout={300}>
                 <Switch location={location}>
                   <Route exact path="/hsl/:h/:s/:l" component={HSL} />
                   <Route exact path="/rgb/:r/:g/:b" component={RGB} />
-                  {/*
-                    Without this 'Route', we get errors during the initial transition from '/' to 'hsl/10/90/50'
+                  {/* Without this `Route`, we get errors during the initial transition from `/` to `/hsl/10/90/50`
                   */}
                   <Route render={() => <div>Not Found</div>} />
                 </Switch>
@@ -57,6 +49,75 @@ const AnimationExample = () => {
             </TransitionGroup>
           </div>
         </div>
-      )} />
+      )}
+    />
   </Router>
-}
+);
+const NavLink = props => (
+  <li style={styles.navItem}>
+    <Link {...props} style={{ color: "inherit" }} />
+  </li>
+);
+const HSL = ({ match: { params } }) => (
+  <div
+    style={{
+      ...styles.fill,
+      ...styles.hsl,
+      background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
+    }}
+  >
+    hsl({params.h}, {params.s}%, {params.l}%)
+  </div>
+);
+const RGB = ({ match: { params } }) => (
+  <div
+    style={{
+      ...styles.fill,
+      ...styles.rgb,
+      background: `rgb(${params.r}, ${params.g}, ${params.b})`
+    }}
+  >
+    rgb({params.r}, {params.g}, {params.b})
+  </div>
+);
+const styles = {};
+styles.fill = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0
+};
+styles.content = {
+  ...styles.fill,
+  top: "40px",
+  textAlign: "center"
+};
+styles.nav = {
+  padding: 0,
+  margin: 0,
+  position: "absolute",
+  top: 0,
+  height: "40px",
+  width: "100%",
+  display: "flex"
+};
+styles.navItem = {
+  textAlign: "center",
+  flex: 1,
+  listStyleType: "none",
+  padding: "10px"
+};
+styles.hsl = {
+  ...styles.fill,
+  color: "white",
+  paddingTop: "20px",
+  fontSize: "30px"
+};
+styles.rgb = {
+  ...styles.fill,
+  color: "white",
+  paddingTop: "20px",
+  fontSize: "30px"
+};
+export default AnimationExample;
